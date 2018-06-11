93f981c66f453357ae1944a4004b76c2792659d1
Mustafa Bandukwala <bandukwalamustafa@gmail.com>
2018-06-10 23:42:03 -0700

    Create SearchInsertPosition.cpp

---
+++ Algorithms/Search Insert Position/SearchInsertPosition.cpp
@@ -0,0 +1,75 @@
+class Solution
+{
+public:
+    int searchInsert(vector<int>& nums, int target)
+    {
+        int low = 0;
+        int high = nums.size() - 1;
+        while(low <= high)
+        {
+            int mid = (high + low) / 2;
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] > target)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return low;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input)
+{
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch)
+    {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input)
+{
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch)
+    {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input)
+{
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim))
+    {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+int stringToInteger(string input)
+{
+    return stoi(input);
+}
+
+int main()
+{
+    string line;
+    while (getline(cin, line))
+    {
+        vector<int> nums = stringToIntegerVector(line);
+        getline(cin, line);
+        int target = stringToInteger(line);
+        
+        int ret = Solution().searchInsert(nums, target);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
