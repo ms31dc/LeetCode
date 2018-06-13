6570835eb61f378375105d4bbeaa5d3ee03eb0d0
Mustafa Bandukwala <bandukwalamustafa@gmail.com>
2018-06-12 18:55:46 -0700

    Create ExcelSheetColumnNumber.cpp

---
+++ Algorithms/Excel Sheet Column Number/ExcelSheetColumnNumber.cpp
@@ -0,0 +1,73 @@
+class Solution
+{
+public:
+    int titleToNumber(string s)
+    {
+        int result = 1, i = 0, output = 0;
+        if(s.length() > 1)
+        {
+            for(int i = 0; i < s.length(); i++)
+            {
+                int count = s.length()-i-1;
+                while(count > 0)
+                {
+                    result = result * 26;
+                    count--;
+                }
+                output = output + (result * (s[i] - 64));
+                result = 1;
+            }
+            return output;
+        }
+        else
+        {
+            return (s[0] - 64);
+        }
+    }
+};
+
+string stringToString(string input)
+{
+    assert(input.length() >= 2);
+    string result;
+    for (int i = 1; i < input.length() -1; i++)
+    {
+        char currentChar = input[i];
+        if (input[i] == '\\') {
+            char nextChar = input[i+1];
+            switch (nextChar)
+            {
+                case '\"': result.push_back('\"'); break;
+                case '/' : result.push_back('/'); break;
+                case '\\': result.push_back('\\'); break;
+                case 'b' : result.push_back('\b'); break;
+                case 'f' : result.push_back('\f'); break;
+                case 'r' : result.push_back('\r'); break;
+                case 'n' : result.push_back('\n'); break;
+                case 't' : result.push_back('\t'); break;
+                default: break;
+            }
+            i++;
+        } 
+        else
+        {
+          result.push_back(currentChar);
+        }
+    }
+    return result;
+}
+
+int main() 
+{
+    string line;
+    while (getline(cin, line))
+    {
+        string s = stringToString(line);
+        
+        int ret = Solution().titleToNumber(s);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
