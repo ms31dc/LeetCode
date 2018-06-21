21c0c2e163ec4555dd365ecc3c9e924348afdfb5
Mustafa Bandukwala <bandukwalamustafa@gmail.com>
2018-06-21 00:05:37 -0700

    Update DistributeCandies.cpp

--- Algorithms/Distribute Candies/DistributeCandies.cpp
+++ Algorithms/Distribute Candies/DistributeCandies.cpp
@@ -12,19 +12,24 @@ public:
     }
 };
 
-void trimLeftTrailingSpaces(string &input) {
-    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+void trimLeftTrailingSpaces(string &input)
+{
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) 
+                                       {
         return !isspace(ch);
     }));
 }
 
-void trimRightTrailingSpaces(string &input) {
-    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+void trimRightTrailingSpaces(string &input)
+{
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) 
+    {
         return !isspace(ch);
     }).base(), input.end());
 }
 
-vector<int> stringToIntegerVector(string input) {
+vector<int> stringToIntegerVector(string input) 
+{
     vector<int> output;
     trimLeftTrailingSpaces(input);
     trimRightTrailingSpaces(input);
@@ -33,15 +38,18 @@ vector<int> stringToIntegerVector(string input) {
     ss.str(input);
     string item;
     char delim = ',';
-    while (getline(ss, item, delim)) {
+    while (getline(ss, item, delim)) 
+    {
         output.push_back(stoi(item));
     }
     return output;
 }
 
-int main() {
+int main()
+{
     string line;
-    while (getline(cin, line)) {
+    while (getline(cin, line)) 
+    {
         vector<int> candies = stringToIntegerVector(line);
         
         int ret = Solution().distributeCandies(candies);
