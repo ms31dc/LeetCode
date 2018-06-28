d413777597616d91ffb826c9c51d2509eb53f9c8
Mustafa Bandukwala <bandukwalamustafa@gmail.com>
2018-06-28 13:46:41 -0700

    Create MaximumSubarray.cpp

---
+++ Algorithms/Maximum Subarray/MaximumSubarray.cpp
@@ -0,0 +1,66 @@
+class Solution 
+{
+public:
+    int maxSubArray(vector<int>& nums) 
+    {
+        int sum=0;
+        int result=INT_MIN;
+        for(int i=0; i<nums.size(); i++)
+        {
+            sum = sum + nums[i];
+            if(result < sum)
+                result = sum;
+            if(sum < 0)
+                sum = 0;
+        }
+        return result;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) 
+{
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) 
+    {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) 
+{
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) 
+    {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) 
+{
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) 
+    {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+int main() 
+{
+    string line;
+    while (getline(cin, line)) 
+    {
+        vector<int> nums = stringToIntegerVector(line);
+        
+        int ret = Solution().maxSubArray(nums);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
