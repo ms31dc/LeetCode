31c27b13200e36ba35c1089f96601680b0cfaa68
Mustafa Bandukwala <bandukwalamustafa@gmail.com>
2018-07-08 23:58:03 -0700

    Create FindAllDuplicatesInAnArray.cpp

---
+++ Algorithms/Find All Duplicates in an Array/FindAllDuplicatesInAnArray.cpp
@@ -0,0 +1,88 @@
+class Solution 
+{
+public:
+    vector<int> findDuplicates(vector<int>& nums) 
+    {
+        int index=0;
+        sort(nums.begin(), nums.end());
+        for(int i=1; i<nums.size(); i++)
+        {
+            if(nums[i] == nums[i-1])
+            {
+                nums[index] = nums[i-1];
+                index++;
+            }
+        }
+        nums.erase(nums.begin()+index, nums.end());
+        return nums;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) 
+{
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) 
+    {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) 
+{
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) 
+    {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) 
+{
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) 
+    {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+string integerVectorToString(vector<int> list, int length = -1) 
+{
+    if (length == -1) 
+    {
+        length = list.size();
+    }
+
+    if (length == 0) 
+    {
+        return "[]";
+    }
+
+    string result;
+    for(int index = 0; index < length; index++)
+    {
+        int number = list[index];
+        result += to_string(number) + ", ";
+    }
+    return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+int main()
+{
+    string line;
+    while (getline(cin, line)) 
+    {
+        vector<int> nums = stringToIntegerVector(line);
+        
+        vector<int> ret = Solution().findDuplicates(nums);
+
+        string out = integerVectorToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
