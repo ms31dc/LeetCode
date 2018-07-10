6861355f1c0c7dcbc7c461c1caf752073b60cc4c
Mustafa Bandukwala <bandukwalamustafa@gmail.com>
2018-07-09 23:26:19 -0700

    Create BestTimeToBuyAndSellStock.cpp

---
+++ Algorithms/Best Time to Buy and Sell Stock/BestTimeToBuyAndSellStock.cpp
@@ -0,0 +1,71 @@
+class Solution 
+{
+public:
+    int maxProfit(vector<int>& prices) 
+    {
+        int maxValue=0;
+        int sum=0;
+        for(int i=1; i<prices.size(); i++)
+        {
+            sum = sum + prices[i];
+            sum = sum - prices[i-1];
+            if(maxValue < sum)
+            {
+                maxValue = sum;
+            }
+            if(sum < 0)
+            {
+                sum=0;
+            }
+        }
+        return maxValue;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) 
+{
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) 
+    {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) 
+{
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) 
+    {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) 
+{
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) 
+    {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+int main() 
+{
+    string line;
+    while (getline(cin, line)) 
+    {
+        vector<int> prices = stringToIntegerVector(line);
+        
+        int ret = Solution().maxProfit(prices);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
