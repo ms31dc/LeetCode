7068aaacb0464f2c4583b57a7a8b9f20074e114f
Mustafa Bandukwala <bandukwalamustafa@gmail.com>
2018-08-09 23:50:12 -0700

    Create FindPeakElement.cpp

---
+++ Algorithms/Find Peak Element/FindPeakElement.cpp
@@ -0,0 +1,74 @@
+class Solution 
+{
+public:
+    int findPeakElement(vector<int>& nums) 
+    {
+        int result=0;
+        bool flag = true;
+        if(nums.size() == 1)
+        {
+            return 0;
+        }
+        for(int i=1; i<nums.size()-1; i++)
+        {
+            if((nums[i] > nums[i-1]) && (nums[i] > nums[i+1]))
+            {
+                result = i;
+                flag = false;
+                break;
+            }
+        }
+        if(flag)
+        {
+            if(nums[0] > nums[1])
+            {
+                return 0;
+            }
+            else
+            {
+                return nums.size()-1;
+            }
+        }
+        return result;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        vector<int> nums = stringToIntegerVector(line);
+        
+        int ret = Solution().findPeakElement(nums);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
