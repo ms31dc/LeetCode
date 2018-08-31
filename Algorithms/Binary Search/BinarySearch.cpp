da9d7afc6ff60b717867f87e50952be7d2b35acb
Mustafa Bandukwala <bandukwalamustafa@gmail.com>
2018-08-30 23:23:41 -0700

    Create BinarySearch.cpp

---
+++ Algorithms/Binary Search/BinarySearch.cpp
@@ -0,0 +1,80 @@
+class Solution 
+{
+public:
+    int search(vector<int>& nums, int target) 
+    {
+        int low = 0, high = nums.size(), mid;
+        while(low <= high)
+        {
+            mid = (low+high)/2;
+            if(nums[mid] == target)
+            {
+                return mid;
+            }
+            else if(nums[mid] > target)
+            {
+                high = mid-1;
+            }
+            else if(nums[mid] < target)
+            {
+                low = mid+1;
+            }
+        }
+        return -1;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) 
+{
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) 
+    {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) 
+{
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) 
+    {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) 
+{
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) 
+    {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+int stringToInteger(string input) 
+{
+    return stoi(input);
+}
+
+int main() 
+{
+    string line;
+    while (getline(cin, line)) 
+    {
+        vector<int> nums = stringToIntegerVector(line);
+        getline(cin, line);
+        int target = stringToInteger(line);
+        
+        int ret = Solution().search(nums, target);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
