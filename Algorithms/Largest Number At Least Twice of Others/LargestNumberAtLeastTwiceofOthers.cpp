bc026ef9c3047d9de744638eb8754156845a961d
Mustafa Bandukwala <bandukwalamustafa@gmail.com>
2018-09-05 23:39:23 -0700

    Create LargestNumberAtLeastTwiceofOthers.cpp

---
+++ Algorithms/Largest Number At Least Twice of Others/LargestNumberAtLeastTwiceofOthers.cpp
@@ -0,0 +1,76 @@
+class Solution
+{
+public:
+    int dominantIndex(vector<int>& nums) 
+    {
+        vector<int> tempArray = nums;
+        sort(tempArray.rbegin(), tempArray.rend());
+        if(tempArray.size() > 1)
+        {
+            if(tempArray[0] >= (2*tempArray[1]))
+            {
+                for(int i=0; i<nums.size(); i++)
+                {
+                    if(nums[i] == tempArray[0])
+                    {
+                        return i;
+                    }
+                }
+            }
+        }
+        else if(tempArray.size() == 1)
+        {
+            return 0;
+        }
+        
+        return -1; 
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) 
+{
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) 
+    {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) 
+{
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) 
+    {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) 
+{
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) 
+    {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+int main() 
+{
+    string line;
+    while (getline(cin, line)) 
+    {
+        vector<int> nums = stringToIntegerVector(line);
+        
+        int ret = Solution().dominantIndex(nums);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
